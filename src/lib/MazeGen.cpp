55589a1cc012fa8ac78a44cc096a42d9620a4d79
derfelterciano <dtvsworld@gmail.com>
2025-05-26 15:31:18 -0700

    started maze generation

---
+++ src/lib/MazeGen.cpp
@@ -0,0 +1,9 @@
+#include "MazeGen.hpp"
+
+#include <algorithm>
+#include <ds/Stack.hpp>
+#include <ds/Vector.hpp>
+#include <random>
+#include <utility>
+
+void gen_maze_dfs(Maze& maze, int start_row, int star_col) {}
---
+++ src/lib/MazeGen.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Maze.hpp"
+
+void gen_maze_dfs(Maze& maze, int start_row = 0, int start_col = 0);
