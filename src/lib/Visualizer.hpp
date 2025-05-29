32b2abe641d250fcbcba3153ef597b6e84c822ed
derfelterciano <dtvsworld@gmail.com>
2025-05-29 14:58:25 -0700

    using cxxopts for arg parsing

---
+++ src/lib/Visualizer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Maze.hpp>
+#include <MazeSolver.hpp>
+#include <ds/Vector.hpp>
+#include <functional>
+
+// draw a maze, no animation needed
+void visualize_maze(const Maze& maze);
+
+// draw the maze generation with animation
+void visualize_maze_generation(
+    Maze&                                                        maze,
+    std::function<void(Maze&, std::function<void(const Maze&)>)> generator = {},
+    int start_row = 0, int start_col = 0);
+
+// draw maze solving with animation
+void visualize_maze_solving(
+    Maze& maze, SolveMethod method,
+    std::function<ds::Vector<vrtx>(const Maze&, SolveMethod,
+                                   std::function<void(const vrtx&)>)>
+        solve_fn = {});
+
+// visualize solution path
+void visualize_solution(const Maze& maze, const ds::Vector<vrtx>& path);
